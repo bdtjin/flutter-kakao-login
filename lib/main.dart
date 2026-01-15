f901ce4ffd53188c2bed8cd0da22461972eb3e90
jin <bdtjin00@gmail.com>
2026-01-15 11:23:40 +0900

    feat: 상자발급 (aka JWT)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk_talk.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk_template.dart';
 
 void main() {
+  KakaoSdk.init (nativeAppKey: "f5c0db234dceada1adea1dc9025e8e28");
   runApp(const MyApp());
 }
 
@@ -56,7 +59,16 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
+  void _incrementCounter()async {
+
+    bool isInstalled = await isKakaoTalkInstalled();
+    print(isInstalled);
+    OAuthToken token = isInstalled
+    ? await UserApi.instance.loginWithKakaoTalk() 
+    : await UserApi.instance.loginWithKakaoAccount();
+
+    print(token.idToken);
+
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
